05681c8748e82569533b91c4d6583184ec1296a4
diegoRM666 <al2202000335@azc.uam.mx>
2024-07-29 20:43:35 -0600

    Update: En el Linux Fedora

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const port = 3000;
 
 // Configura el motor de plantillas EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join('/Users/mariafernandamanriquezangeles/Desktop/Escuela/SS/dashboardUAM/', 'views'));
+app.set('views', path.join('/home/mrcubeoner/Desktop/ServicioSocial/dashboardUAM', 'views'));
 
 // Conexión a la base de datos
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '15122121B',
-  database: 'covid19'
+  password: 'gogo219715122121B$',
+  database: 'mydb'
 });
 
 // Ruta para obtener los datos de la base de datos y renderizar la vista
 app.get('/', async (req, res) => {
   try {
-    const [rows] = await pool.execute('SELECT count(*) as num FROM covid19.caso;');
-    res.render('index', { casos: rows });
+    const [rows] = await pool.execute('SELECT count(*) as num FROM profesor;');
+    res.render('index', { profesores: rows });
   } catch (error) {
     console.error('Error al obtener los casos:', error);
     res.status(500).send('Error en el servidor');
